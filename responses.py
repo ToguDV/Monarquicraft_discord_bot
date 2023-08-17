f63a01397025b6ccdbc123447ca2576efa83d3a8
Togu <timeofgetup@gmail.com>
2023-08-17 15:12:02 -0500

    test

--- responses.py
+++ responses.py
@@ -4,7 +4,7 @@ import random
 def get_response(message) -> str:
     p_message = message.lower()
     if p_message == 'hello':
-        return 'Hey there!'
+        return 'Hey there!!!!!!'
 
     if p_message == 'roll':
         return str(random.randint(1, 6))
